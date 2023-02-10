917776978bc1b4ba4101f349a83961ca7e452757
Luan Oliveira <Luanoliveirags@gmail.com>
2023-02-10 10:55:20 -0300

    menu

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "img/iconMenu.png";
+        document.querySelector('.icon').src = "img/Menu2.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "img/iconMenu.png";
+        document.querySelector('.icon').src = "img/Menu2.png";
     }
 }
